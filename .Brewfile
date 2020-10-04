deeca353946a7c854958fb69e8f87504ebf02c9c
Vitor Pellegrino <pellegrino@linux.com>
2020-10-04 21:40:24 +0200

    Simplifying fish boot

--- .Brewfile
+++ .Brewfile
@@ -1,5 +1,6 @@
 tap "homebrew/bundle"
 tap "homebrew/cask"
+tap "homebrew/cask-fonts"
 tap "homebrew/cask-versions"
 tap "homebrew/core"
 # Load/unload environment variables based on $PWD
@@ -14,14 +15,34 @@ brew "fortune"
 brew "fzf"
 # GNU Pretty Good Privacy (PGP) package
 brew "gnupg"
+# Open source programming language to build simple/reliable/efficient software
+brew "go"
 # Manage your Java environment
 brew "jenv"
+# Mac App Store command-line interface
+brew "mas"
+# Ambitious Vim-fork focused on extensibility and agility
+brew "neovim"
+# Manage multiple Node.js versions
+brew "nvm"
+# Terminal multiplexer
+brew "tmux"
 # Yet Another Dotfiles Manager
 brew "yadm"
 cask "alacritty"
+cask "android-sdk"
+cask "corretto"
+cask "corretto8"
 cask "firefox"
+cask "flutter"
+cask "font-jetbrains-mono"
+cask "iterm2"
 cask "karabiner-elements"
+cask "notion"
 cask "slack"
 cask "slate"
+cask "spotify"
 cask "telegram-desktop"
 cask "visual-studio-code"
+mas "Todoist", id: 585829637
+mas "Xcode", id: 497799835
--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -3,38 +3,22 @@
 ### Variables
 # Golang developers might need this one
 set -xg GOPATH $HOME
-
 set -xg GPG_TTY (tty) 
-
-set -xg ANDROID_SDK_HOME /opt/android-sdk
-set -xg ANDROID_HOME  $ANDROID_SDK_HOME
-
 ### Path extensions
 
-set -U fish_user_paths $HOME/bin $fish_user_paths
-set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-set -U fish_user_paths /usr/local/bin $fish_user_paths
-set -U fish_user_paths $GOPATH/bin $fish_user_paths
-set -U fish_user_paths $HOME/tools/flutter/bin
-set -U fish_user_paths $HOME/tools/adr-tools/src $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/cmdline-tools/tools/bin $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/emulator $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/platform-tools $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/tools/bin $fish_user_paths
-set -U fish_user_paths $HOME/.jenv/bin $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/cmdline-tools/tools $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/cmdline-tools/tools/bin $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/emulator $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/platform-tools $fish_user_paths
-set -U fish_user_paths $ANDROID_SDK_HOME/tools/bin $fish_user_paths
-
-
+set -x PATH $PATH $HOME/bin \
+	# $ANDROID_SDK_HOME/cmdline-tools/tools \
+	#$ANDROID_SDK_HOME/cmdline-tools/tools/bin \
+	#$ANDROID_SDK_HOME/emulator \
+	#$ANDROID_SDK_HOME/platform-tools \
+	#$ANDROID_SDK_HOME/tools/bin \
+	$HOME/.jenv/bin \
+	$HOME/tools/adr-tools/src \
+	$GOPATH/bin \
+	$HOME/.cargo/bin \
+	$HOME/.local/bin 
 ### Init scripts
-
-fortune 
 eval (direnv hook fish)
-
 
 fish_vi_key_bindings
 
