d80448f23225a892cfbfcc36f034943aa3b4ecea
Vitor Pellegrino <pellegrino@linux.com>
2022-12-08 16:01:33 +0100

    feat(zsh): configuring docker host and pyenv

---
+++ .config/zsh/zshrc.d/docker-host.zsh
@@ -0,0 +1,4 @@
+# Configuring docker-cli to point to limactl based docker DOCKER_HOST
+# @see https://gist.github.com/redpeacock78/6a8c476b42828bb61bec9e7f39013a82 
+export DOCKER_HOST=unix://$HOME/docker.sock
+
